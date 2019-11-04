805a1f97bbdcd708ce01502a473c228af278a01d
tristansiquoir <archtemps@hotmail.fr>
2019-11-04 11:23:19 +0100

    finaly_start

--- views/books.php
+++ views/books.php
@@ -2,18 +2,38 @@
 <?php ob_start(); ?>
 <h1>Liste des titres</h1>
 
-<p>HOHHOOOHOHOHOHOHOHOHOHOHOHOHOHOHOHOH</p>
+<div class="contenairs">
+    <div class="row">
 
-<ul>
     <?php
-        foreach ($books as $book) {
-            ?>
-            <li><?php echo $book['title']; ?></li>
-            <?php
+        foreach ($books as $book) {       
+    ?>
+        
+    <div class="col md-3 mt-4">
+        <div class="card-body">
+            <div class="card-image">
+                <img src="public/<?php echo $book ["imageLink"]; ?> " alt="<?php echo $book ['title']; ?> ">
+            </div>
+            <h2><?php echo $book['title']; ?></h2>   
+            
+            <div>
+            </div>
+            
+            
+            <div class="card-footer text-muted">
+                <a class="btn btn-primary" href="<?php echo $book["link"];?> "> Informaton </a>
+            </div>           
+        </div>            
+    </div>
+      
+            
+            
+            
+    <?php
         }
     ?>
-</ul>
-
+    </div>
+</div>
 
 <?php $content = ob_get_clean(); ?>
 <?php require('public/index.php'); ?>
