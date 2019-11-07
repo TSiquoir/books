bfc37c7a984630b6e4c666754e01e16769cc7106
tristansiquoir <archtemps@hotmail.fr>
2019-11-07 17:03:14 +0100

    formulaire_php

--- admin/index.php
+++ admin/index.php
@@ -1,13 +1,81 @@
-<?php>
+<?php
     $db = new PDO('mysql:host=localhost;dbname=books', 'root');
+
+    $id = isset($_GET['id']) ? (int) $_GET['id'] : NULL;
+
+    if ($id) {
+        $stmt = $db->prepare('SELECT * FROM books WHERE id = :id');
+      
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+
+        $stmt->execute();
+
+        $book = $stmt->fetch();
+        
+        
+    }
 
     $stmt = $db->prepare('SELECT * FROM authors Order BY name');
     
     $stmt->execute();
   
     $authors = $stmt->fetchAll(PDO::FETCH_ASSOC);
+;
+    // Seulement quand le formulaire est posté
+    if (isset($_POST['book'])) {
 
-    var_dump($authors);
+        $title = (string) $_POST['title'];
+        $description = (string) $_POST['description'];
+        $author = (int) $_POST['author_id'];
+        $pages = (string) $_POST['pages'];
+        $wikipedia_link = (string) $_POST['wikipedia_link'];
+        $year = (string) $_POST['year'];
+        $language = (string) $_POST['language'];
+        $country = (string) $_POST['country'];
+
+        if (!preg_match('/^(http|https):\/\/([a-z]{2})\.wikipedia\.org\/([a-zA-Z0-9-_\/%:]+)?/i', $wikipedia_link)) {
+            $wikipedia_link = '';
+        }
+    
+
+        // INSERT INTO books (title, description, author_id, pages, `year`, `language`, country) VALUES ("Titre de test", "xxxxxxxx", 1, 211, 1980, "Français", "France")
+        $stmt = $db->prepare('INSERT INTO 
+            `books`(
+                `title`,
+                `description`,
+                `author_id`,
+                `pages`,
+                `year`,
+                `language`,
+                `country`,
+                `wikipedia_link`,
+            )
+            VALUE (
+                :title,
+                :description,
+                :author_id,
+                :pages,
+                :year,
+                :language,
+                :country,
+                :wikipedia_link,
+            )');
+            $stmt->bindParam(':title', $title, PDO::PARAM_STR);
+            $stmt->bindParam(':description', $description, PDO::PARAM_STR);
+            $stmt->bindParam(':author_id', $author_id, PDO::PARAM_INT);
+            $stmt->bindParam(':pages', $pages, PDO::PARAM_INT);
+            $stmt->bindParam(':year', $year, PDO::PARAM_INT);
+            $stmt->bindParam(':country', $country, PDO::PARAM_STR);
+            $stmt->bindParam(':wikipedia_link', $wikipedia_link, PDO::PARAM_STR);
+            $stmt->bindParam(':language', $language, PDO::PARAM_STR);
+
+            $stmt->execute();
+
+            $id = $db->lastInsertId();
+
+    }
+
+ 
 ?>
 
 
@@ -28,19 +96,22 @@
 </head>
 <body>
 <div class="container">
-    <form action="./" methode="post">
+    <h1>Ajouter un livre</h1>
+    <form action="./" method="post">
         <div class="row">
             <div class="col-md-6">
             
                 <div class="form-group">
                     <label for="title">Titre</label>
-                    <input maxlength="10" type="text" class="form-control" id="title" aria-describedby="titleHelp" placeholder="Titre du livre">
+                    <input name="title" value="<?php echo isset($book) ? $book['title']: ''; ?>" maxlength="10" type="text" class="form-control" id="title" aria-describedby="titleHelp" placeholder="Titre du livre">
                     <small id="titleHelp" class="form-text text-muted">Titre du livre entre 0 et 255 caractéres.</small>
                 </div>
 
                 <div class="form-group">
                     <label for="description">Description</label>
-                    <textarea name="description" class="form-control" id="description" rows="3"></textarea>
+                    <textarea name="description" class="form-control" id="description" rows="3">
+                        <?php echo isset($book) ? $book['description']: ''; ?>
+                    </textarea>
                 </div>
 
                 <div class="form-group">
@@ -48,20 +119,58 @@
                     <select name="author_id" class="form-control" id="author_id">
                     
                     <?php foreach ($authors as $author) { ?>
-                    <option> value="<?php echo $author['id']; ?>">
-                        <?php echo $author['name']; ?>
-                    </option>    
+                        <option <?php echo (isset($book) && $book['author_id'] === $author['id']) ? 'selected' : ''; ?> value="<?php echo $author['id']; ?>">
+                            <?php echo $author['name']; ?>
+                        </option>
+
+                     
                     <?php } ?>
                     
                
                     </select>
                 </div>
+
+                <div class="form-group">
+                    <label for="pages">Nombre de pages</label>
+                    <input name="pages" type="number" step="1" min="0" class="form-control" id="pages">
+                    
+                </div>
             </div>
 
             <div class="col-md-6">
-            col 6
+            
+                <div class="form-group">
+                    <label for="wikipedia_link">Lien wikipedia</label>
+                    <input name="wikipedia_link" type="text" class="form-control" id="wikipedia_link">
+                        
+                </div>
+
+                <div class="form-group">
+                    <label for="year">Année de parution</label>
+                    <input name="year" type="number" step="1" min="0" class="form-control" id="year">
+                    
+                </div>
+
+                <div class="form-group">
+                    <label for="language">Langue</label>
+                    <input name="language" type="text" class="form-control" id="language">
+                    
+                </div>
+
+                <div class="form-group">
+                    <label for="country">Pays</label>
+                    <input name="country" type="text" class="form-control" id="country">
+                    
+                </div>
+
             </div>
         </div>
+
+        <div clas="row">
+            <div class="col-md-12">
+                <button name="book" type="submit" class="btn btn-primary">Valider</button>
+            </div>
+        <div>
     </form>
 </div>
     
